2775c270930e6b63a91fd4f0eb9f34a53f257ab8
tega <tega.philip@gmail.com>
2017-08-10 18:59:18 +0200

    added new recipes

---
+++ sample/recipes/deploy.rb
@@ -0,0 +1,4 @@
+node[:deploy].each do |app_name, deploy|
+	Chef::Log.info("Node Deploy is ===>")
+	Chef::Log.info(deploy)
+	end
--- sample/recipes/test.rb
+++ sample/recipes/test.rb
@@ -1,13 +1,3 @@
-Chef::Log.info("Node is ===>")
-Chef::Log.info(node)
-
-
-node[:deploy].each do |app_name, deploy|
-	Chef::Log.info("Node Deploy is ===>")
-	Chef::Log.info(deploy)
-	end
-
-
 case node[:platform]
 when "ubuntu","debian"
   package "docker.io" do
