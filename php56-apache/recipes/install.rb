0fc374ca057472c4b8e7230dfa6e6e95b55cabba
tega <tega.philip@gmail.com>
2017-08-11 11:57:16 +0200

    added new php packages to install recipe

--- php56-apache/recipes/install.rb
+++ php56-apache/recipes/install.rb
@@ -1,3 +1,23 @@
 package value_for_platform_family(:rhel => 'httpd', :debian => 'apache2') do
 	action :install
 end
+
+packages = [
+	'php5.6',
+	'php5.6-mysql',
+	'php5.6-gettext',
+	'php5.6-mbstring',
+	'php5.6-xdebug',
+	'libapache2-mod-php5.6',
+	'php5.6-curl',
+	'php5.6-zip',
+	'php5.6-dom',
+	'php5.6-gd'
+]
+
+packages.each { |package_name|
+	Chef::Log.info("Installing package : #{package_name}")
+	package "#{package_name}" do
+		action 'install'
+	end
+}
