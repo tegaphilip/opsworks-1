3ecda1292741988d1b33afb107c8c2912470ac44
tega <tega.philip@gmail.com>
2017-08-11 12:44:35 +0200

    Bug fix method name error

--- php56-apache/recipes/disable_signatures.rb
+++ php56-apache/recipes/disable_signatures.rb
@@ -1,9 +1,9 @@
 if File.exist?('/etc/apache2/apache2.conf')
-	Chef::log::info('Writing to apache2.conf file')
+	Chef::Log::info('Writing to apache2.conf file')
 	open('/etc/apache2/apache2.conf', 'a') do |f|
   	f.puts "ServerSignature Off\n"
 		f << "ServerTokens Prod\n"
 	end
 else
-	Chef::log::info('apache2.conf file not found')
+	Chef::Log::info('apache2.conf file not found')
 end
