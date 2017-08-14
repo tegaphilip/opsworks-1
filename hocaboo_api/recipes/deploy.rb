f13fbcb79636c062b64dd31288c89637c6a2c7e4
tega <tega.philip@gmail.com>
2017-08-15 01:21:03 +0200

    .

--- hocaboo_api/recipes/deploy.rb
+++ hocaboo_api/recipes/deploy.rb
@@ -1,29 +1,13 @@
-deploy '/var/www/html/hocaboo-api' do
-  # repository 'https://github.com/tegaphilip/fdc.git'
+username = node[:deploy][:user]
+
+deploy 'App' do
+  user username
   repository 'git@gitlab.com:hocaboo/api.git'
   revision 'hocaboolive'
   keep_releases 5
   migrate false
   ignore_failure false
   symlink_before_migrate ({})
-
-  # before_migrate do
-  #   Chef::Log::info("Current release path is " + release_path)
-  #   %w{ /var/www/html/hocaboo-api/shared  /var/www/html/hocaboo-api/shared/config }.each do |dir|
-  #     directory dir do
-  #       mode '0755'
-  #       action :create
-  #     end
-  #
-  #     file '/var/www/html/hocaboo-api/shared/config/database.yml' do
-  #       mode '0755'
-  #       action :create
-  #     end
-  #   end
-  #
-  # end
-  # user 'ubuntu'
-  # deploy_to '/var/www/html/hocaboo-api'
-  # ssh_wrapper '/tmp/private_code/wrap-ssh4git.sh'
+  deploy_to '/var/www/html/hocaboo-api'
   action :deploy
 end
--- initial_setup/recipes/generate_ssh_keys.rb
+++ initial_setup/recipes/generate_ssh_keys.rb
@@ -1,5 +1,11 @@
 username = node[:deploy][:user]
 private_key_path = "/home/#{node[:deploy][:user]}/.ssh/id_rsa"
+public_key_path = "/home/#{node[:deploy][:user]}/.ssh/id_rsa.pub"
+
+send_to_slack = false
+unless File.exist?("#{private_key_path}")
+  send_to_slack = true
+end
 
 bash 'generate ssh key pair' do
   user username
@@ -7,4 +13,23 @@ bash 'generate ssh key pair' do
     ssh-keygen -t rsa -q -f #{private_key_path} -P ""
     EOH
   not_if { ::File.exist?("#{private_key_path}") }
+end
+
+file = File.open(public_key_path, "rb")
+contents = file.read
+
+Chef::Log::info("File Exists?")
+Chef::Log::info(send_to_slack.to_s)
+
+message = {'text' => contents}
+command_string = "curl -X POST -H 'Content-type: application/json' --data '" + message.to_json
+ + "' " + node[:hocaboo][:slack][:hooks_url]
+
+Chef::Log::info("File Exists?")
+Chef::Log::info(send_to_slack.to_s)
+Chef::Log::info(command_string)
+
+execute 'Send Public Key to Slack' do
+  command command_string
+  action :run
 end
