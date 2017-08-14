ce6172ce5efb58ecc95846b747c6a0d50bcb909b
tega <tega.philip@gmail.com>
2017-08-14 18:28:46 +0200

    .

--- initial_setup/recipes/generate_ssh_keys.rb
+++ initial_setup/recipes/generate_ssh_keys.rb
@@ -1,9 +1,18 @@
+if File.exist?('~/.ssh/id_rsa.pub')
+	Chef::Log::info('I see am')
+else
+	Chef::Log::info('I no see am')
+end
+
 bash 'extract_module' do
   code <<-EOH
     ssh-keygen -t rsa -q -f ~/.ssh/id_rsa -P ""
     EOH
-  not_if { ::File.exist?('~/.ssh/id_rsa.pub') }
+  not_if { ::File.exist?('/root/.ssh/id_rsa.pub') }
 end
+
+
+
 
 # file = File.open("~/.ssh/id_rsa.pub", "rb")
 # contents = file.read
