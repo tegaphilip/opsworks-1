19204dcccf15cca204f887233b3a72b1cd9f3594
tega <tega.philip@gmail.com>
2017-08-15 00:53:41 +0200

    .

--- php56_apache/recipes/install_composer.rb
+++ php56_apache/recipes/install_composer.rb
@@ -2,6 +2,6 @@ username = node[:deploy][:user]
 
 execute 'Install Composer' do
   command 'curl -s https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer'
-  action :install
+  action :run
   not_if 'which composer'
 end
