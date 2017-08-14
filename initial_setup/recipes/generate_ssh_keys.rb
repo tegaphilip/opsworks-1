9d1b2e3d21a3b24b037e6018e048d4ff48a34717
tega <tega.philip@gmail.com>
2017-08-14 17:54:37 +0200

    .

--- initial_setup/recipes/generate_ssh_keys.rb
+++ initial_setup/recipes/generate_ssh_keys.rb
@@ -1,13 +1,12 @@
-bash 'extract_module' do
-  code <<-EOH
-    ssh-keygen -t rsa -q -f ~/.ssh/id_rsa -P ""
-    EOH
-  not_if { ::File.exist?('~/.ssh/id_rsa.pub') }
-end
-
-
-# execute "generate ssh skys for #{username}." do
+# bash 'generate ssh keys' do
+#   code <<-EOH
+#     ssh-keygen -t rsa -q -f ~/.ssh/id_rsa -P ""
+#     EOH
+#   not_if { ::File.exist?('~/.ssh/id_rsa.pub') }
+# end
+execute "generate ssh key" do
     # user username
     # creates "/home/#{username}/.ssh/id_rsa.pub"
-    # command "ssh-keygen -t rsa -q -f /home/#{username}/.ssh/id_rsa -P \"\""
-# end
+    command "ssh-keygen -t rsa -q -f ~/.ssh/id_rsa -P \"\""
+    not_if { ::File.exist?('~/.ssh/id_rsa.pub') }
+end
