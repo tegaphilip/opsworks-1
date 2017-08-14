6d6d967b7989d4dd5f98d6fdc1819b70692ed6a2
tega <tega.philip@gmail.com>
2017-08-14 11:11:22 +0200

    moved packages into attributes file

--- php56-apache/attributes/default.rb
+++ php56-apache/attributes/default.rb
@@ -13,3 +13,22 @@
 #
 # See also: http://docs.aws.amazon.com/opsworks/latest/userguide/customizing.html
 ###
+
+packages = [
+	'php5.6',
+	'php5.6-mysql',
+	'php5.6-gettext',
+	'php5.6-mbstring',
+	'php5.6-xdebug',
+	'libapache2-mod-php5.6',
+	'php5.6-curl',
+	'php5.6-zip',
+	'php5.6-dom',
+	'php5.6-gd',
+  'mysql-client'
+  #'mysql-server' #needed for mysql client
+]
+
+default[:php56-apache][:packages] = packages
+
+include_attribute "php56-apache::customize"
--- php56-apache/recipes/install.rb
+++ php56-apache/recipes/install.rb
@@ -15,22 +15,22 @@ package value_for_platform_family(:rhel => 'httpd', :debian => 'apache2') do
 	action :install
 end
 
-packages = [
-	'php5.6',
-	'php5.6-mysql',
-	'php5.6-gettext',
-	'php5.6-mbstring',
-	'php5.6-xdebug',
-	'libapache2-mod-php5.6',
-	'php5.6-curl',
-	'php5.6-zip',
-	'php5.6-dom',
-	'php5.6-gd',
-  'mysql-client'
-  #'mysql-server' #needed for mysql client
-]
+# packages = [
+# 	'php5.6',
+# 	'php5.6-mysql',
+# 	'php5.6-gettext',
+# 	'php5.6-mbstring',
+# 	'php5.6-xdebug',
+# 	'libapache2-mod-php5.6',
+# 	'php5.6-curl',
+# 	'php5.6-zip',
+# 	'php5.6-dom',
+# 	'php5.6-gd',
+#   'mysql-client'
+#   #'mysql-server' #needed for mysql client
+# ]
 
-packages.each { |package_name|
+node[:php56-apache][:packages].each { |package_name|
 	Chef::Log.info("Installing package : #{package_name}")
 	package "#{package_name}" do
 		action 'install'
