94d92e2b5068d2425336243df206b95326222deb
tega <tega.philip@gmail.com>
2017-08-14 18:09:00 +0200

    .

--- initial_setup/recipes/generate_ssh_keys.rb
+++ initial_setup/recipes/generate_ssh_keys.rb
@@ -1,12 +1,12 @@
-# bash 'generate ssh keys' do
-#   code <<-EOH
-#     ssh-keygen -t rsa -q -f ~/.ssh/id_rsa -P ""
-#     EOH
-#   not_if { ::File.exist?('~/.ssh/id_rsa.pub') }
-# end
-execute "generate ssh key" do
-    # user username
-    # creates "/home/#{username}/.ssh/id_rsa.pub"
-    command "ssh-keygen -t rsa -q -f ~/.ssh/id_rsa -P \"\""
-    not_if { ::File.exist?('~/.ssh/id_rsa.pub') }
+bash 'extract_module' do
+  code <<-EOH
+    ssh-keygen -t rsa -q -f ~/.ssh/id_rsa -P ""
+    EOH
+  not_if { ::File.exist?('~/.ssh/id_rsa.pub') }
 end
+
+file = File.open("~/.ssh/id_rsa.pub", "rb")
+contents = file.read
+
+Chef::Log::info("Public key is")
+Chef::Log::info(contents)
